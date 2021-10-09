a92a0cd1a04d080de56a916c8b9bb5bb2d389ae6
willardsigei <kipngeno.sigei@student.moringaschool.com>
2021-10-10 00:52:48 +0300

    forms binding

--- src/app/create-quote/create-quote.component.ts
+++ src/app/create-quote/create-quote.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quotes } from '../quotes';
 
 @Component({
   selector: 'app-create-quote',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-quote.component.css']
 })
 export class CreateQuoteComponent implements OnInit {
+  newQuote = new Quotes('','','',0,0,'');
+    submitQuote(){
+      
+    }
 
   constructor() { }
 

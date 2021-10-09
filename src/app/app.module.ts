325f7fc41ab3a5aa92f2fc929852fee3f59f8854
willardsigei <kipngeno.sigei@student.moringaschool.com>
2021-10-09 11:53:35 +0300

    created components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuoteBoxComponent } from './quote-box/quote-box.component';
+import { QuotesComponent } from './quotes/quotes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuoteBoxComponent,
+    QuotesComponent,
   ],
   imports: [
     BrowserModule,
